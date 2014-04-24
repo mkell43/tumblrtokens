ac4b2785718b5e8265a46e948a74c02cc6f25bd6
Mike Keller <mike.k@blu-web.com>
2014-04-23 20:04:06 -0500

    Cleaned up non-functioning code.

--- tumblrtokens/__init__.py
+++ tumblrtokens/__init__.py
@@ -2,17 +2,6 @@ import oauth2
 import urlparse
 
 
-class Consumer:
-
-    """
-        Creates a new consumer for authentication with Tumblr.
-    """
-
-    def __init__(self, consumer_key, consumer_secret):
-
-        return oauth2.Consumer(consumer_key, consumer_secret)
-
-
 def consumer(consumer_key, consumer_secret):
 
     """
@@ -20,24 +9,6 @@ def consumer(consumer_key, consumer_secret):
     """
 
     return oauth2.Consumer(consumer_key, consumer_secret)
-
-
-class RequestToken:
-
-    """
-        Creates a new request token for gaining credentials to authenticate with
-         Tumblr.
-    """
-
-    def __init__(self, consumer):
-
-        request_token_url = 'http://www.tumblr.com/oauth/request_token'
-
-        client = oauth2.Client(consumer)
-
-        resp, content = client.request(request_token_url, 'POST')
-
-        self.token = urlparse.parse_qs(content)
 
 
 def request_token(consumer):
@@ -56,29 +27,6 @@ def request_token(consumer):
     return urlparse.parse_qs(content)
 
 
-class AccessToken:
-
-    """
-        Creates a new access token for gaining credentials to authenticate with
-         Tumblr.
-    """
-
-    def __init__(self, consumer, request_response, request_token):
-
-        access_token_url = 'http://www.tumblr.com/oauth/access_token'
-
-        url = urlparse.urlparse(request_response)
-        query_dict = urlparse.parse_qs(url.query)
-        oauth_verifier = query_dict['oauth_verifier'][0]
-
-        token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'][0])
-        token.set_verifier(oauth_verifier)
-        client = oauth2.Client(consumer)
-
-        resp, content = client.request(access_token_url, 'POST')
-        self.token = urlparse.parse_qs(content)
-
-
 def access_token(consumer, request_response, request_token):
 
     """
@@ -94,7 +42,7 @@ def access_token(consumer, request_response, request_token):
 
     token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'][0])
     token.set_verifier(oauth_verifier)
-    client = oauth2.Client(consumer)
+    client = oauth2.Client(consumer, token)
 
     resp, content = client.request(access_token_url, 'POST')
     return urlparse.parse_qs(content)
