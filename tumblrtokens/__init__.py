7f7d24a45cdae66b5d84da584d4e38639f9d657d
Mike Keller <mike.k@blu-web.com>
2014-04-23 19:39:46 -0500

    Updated __init__.py

--- tumblrtokens/__init__.py
+++ tumblrtokens/__init__.py
@@ -13,6 +13,15 @@ class Consumer:
         return oauth2.Consumer(consumer_key, consumer_secret)
 
 
+def consumer(consumer_key, consumer_secret):
+
+    """
+        Creates a new consumer for authentication with Tumblr.
+    """
+
+    return oauth2.Consumer(consumer_key, consumer_secret)
+
+
 class RequestToken:
 
     """
@@ -29,6 +38,22 @@ class RequestToken:
         resp, content = client.request(request_token_url, 'POST')
 
         self.token = urlparse.parse_qs(content)
+
+
+def request_token(consumer):
+
+    """
+        Creates a new request token for gaining credentials to authenticate with
+         Tumblr.
+    """
+
+    request_token_url = 'http://www.tumblr.com/oauth/request_token'
+
+    client = oauth2.Client(consumer)
+
+    resp, content = client.request(request_token_url, 'POST')
+
+    return urlparse.parse_qs(content)
 
 
 class AccessToken:
@@ -52,3 +77,24 @@ class AccessToken:
 
         resp, content = client.request(access_token_url, 'POST')
         self.token = urlparse.parse_qs(content)
+
+
+def access_token(consumer, request_response, request_token):
+
+    """
+        Creates a new access token for gaining credentials to authenticate with
+         Tumblr.
+    """
+
+    access_token_url = 'http://www.tumblr.com/oauth/access_token'
+
+    url = urlparse.urlparse(request_response)
+    query_dict = urlparse.parse_qs(url.query)
+    oauth_verifier = query_dict['oauth_verifier'][0]
+
+    token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'][0])
+    token.set_verifier(oauth_verifier)
+    client = oauth2.Client(consumer)
+
+    resp, content = client.request(access_token_url, 'POST')
+    return urlparse.parse_qs(content)
